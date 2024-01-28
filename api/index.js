f95e233f4fe489a63640c75f083c9e3d19f20690
Vadym <vadym_v@ukr.net>
2024-01-28 20:06:13 +0200

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-app.listen(8080, () => {
-  console.log('Server is running on port 8080');
-});
+const run = () => {
+  try {
+    mongoose.connect(process.env.DB_URI);
+    console.log('Connected to MongoDB');
+
+    app.listen(8080, () => {
+      console.log('Server is running on port 8080');
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+run();
