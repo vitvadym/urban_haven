b7b571c24c5200d21ac4295c1f5e31618f20bfc7
Vadym <vadym_v@ukr.net>
2024-01-29 17:17:39 +0200

    add user model

---
+++ api/models/user.model.js
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default: '/client/src/assets/avatar.jpg',
+    },
+  },
+  { timestamps: true },
+);
+
+const User = model('User', userSchema);
+
+export default User;
